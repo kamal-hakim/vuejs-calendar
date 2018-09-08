df73bd7db1fd2eca6ee54164b1e6023a483dc3cb
Kamal Hakim <kamalhakim.zainal@gmail.com>
2018-09-08 12:34:05 +0800

    Setting up the calendar

--- src/web.entry.js
+++ src/web.entry.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
+import Vue from 'vue';
+import App from './components/App.vue';
+import './style.scss';
+
+import moment from 'moment-timezone';
+moment.tz.setDefault('Asia/Kuala_Lumpur');
+Object.defineProperty(Vue.prototype, '$moment', {get() { return this.$root.moment }})
 
 new Vue({
   el: '#app',
   data: {
-    msg: 'Hello World'
-  }
+    moment,    
+  },
+  components: {
+    App
+  },
 });
